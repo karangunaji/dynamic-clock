32d64ccfcd5835cef14cd4ea4184654ce779d39b
karan rathod <92873841+karangunaji@users.noreply.github.com>
2023-01-18 21:56:15 +0530

    Add files via upload

---
+++ app3.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+const btn = document.getElementById('btn');
+const show = document.getElementById("show");
+
+btn.addEventListener('click' , () => {
+    
+    show.classList.toggle('more')
+    
+})
+
